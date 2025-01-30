7e02782f014f6f939fe6227cceb866810d61e8d1
Baywatch Rambotics <45979505+FRC7790@users.noreply.github.com>
2025-01-30 11:37:24 -0500

    Update ShooterPivot.java

--- src/main/java/frc/robot/subsystems/Coral/ShooterPivot.java
+++ src/main/java/frc/robot/subsystems/Coral/ShooterPivot.java
@@ -1,8 +1,39 @@
 package frc.robot.subsystems.Coral;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterPivotConstants;
 
 public class ShooterPivot extends SubsystemBase {
 
+    private boolean isInitialized = false;
 
+    private float desiredAngle;
+
+    public float NormalizeAngle(float angle)
+    {
+        float newAngle = angle - ShooterPivotConstants.angleOffset;
+
+         while(newAngle > 180)
+         {
+            newAngle -= 360;
+         }
+
+         while(newAngle < -180)
+         {
+            newAngle += 360;
+         }
+        return newAngle;
+        }
+
+        public ShooterPivot(){
+
+
+    }
+    @Override
+        public void periodic() {
+            if (!this.isInitialized) {
+                this.desiredAngle = NormalizeAngle((float)(this.pivotEncoder.getAbsolutePosition().getValue()*360));
+                this.isInitialized = true;
+            }
+    }
 }
