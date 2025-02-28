41a950fb02cb252f27464913d33b28d2b1dabe00
Baywatch Rambotics <frc@harborps.org>
2025-02-28 16:31:01 -0500

    Revert "Lots"
    This reverts commit a5fee28ee0250545f6daa5939122f382dcc1d11c.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.ButtonBox;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as
@@ -23,7 +22,6 @@ public class Robot extends TimedRobot
   private        Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-  private ButtonBox    buttonBox;
 
   private Timer disabledTimer;
 
@@ -101,9 +99,6 @@ public class Robot extends TimedRobot
   @Override
   public void autonomousInit()
   {
-
-    buttonBox.clearTargets();
-    
     m_robotContainer.setMotorBrake(true);
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
@@ -125,7 +120,6 @@ public class Robot extends TimedRobot
   @Override
   public void teleopInit()
   {
-    buttonBox.clearTargets();
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
