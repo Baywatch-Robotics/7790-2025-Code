1bc50a525b89ad10d938d53617a55c61cffb0741
CheaterCJ <frc@harborps.org>
2025-09-17 17:27:51 -0500

    Update Configs.java

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -57,7 +57,7 @@ public final class Configs {
     public static final SparkMaxConfig ArmConfig = new SparkMaxConfig();
 
     static {
-      ArmConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(40).voltageCompensation(12);
+      ArmConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(80).voltageCompensation(12);
       /*
        * Configure the closed loop controller. We want to make sure we set the
        * feedback sensor as the primary encoder.
